c1d325055620af7464c3fb037c2b4bf19ee3c27c
Mark Wireman <mark_wireman@hotmail.com>
2024-11-02 20:04:46 -0400

    Updated initialization of account to pass in two floats

--- cd_account.py
+++ cd_account.py
@@ -17,7 +17,7 @@ def create_cd_account(balance, interest_rate, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
-    account = Account(0,0.0)
+    account = Account(0.0,0.0)
 
     # Calculate interest earned
     # ADD YOUR CODE HERE
